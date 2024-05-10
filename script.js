1756a0d45e1d8a59963ffac611e25ce164d7f180
soliha <soliha7895@gamil.com>
2024-05-10 15:50:55 +0500

    comment nomi

---
+++ script.js
@@ -0,0 +1,15 @@
+// let num1 = +prompt("1-sonni kiriting")
+// let num2 = +prompt("2-sonni kiriting")
+// let num3 = +prompt("3-sonni kiriting")
+
+
+// // document.write(num1 + " * " + num2 + " = " + num1 * num2 + "<br>");
+// document.write(`${num1} + ${num2} + ${num3} = ${num1 + num2 + num3} <br>`);
+// document.write(`${num1} - ${num2} - ${num3} = ${num1 - num2 - num3} <br>`);
+// document.write(`${num1} * ${num2} * ${num3} = ${num1 * num2 * num3} <br>`);
+// document.write(`${num1} / ${num2} / ${num3} = ${num1 / num2 / num3} <br>`);
+
+let age = +prompt("yoshizni kiriting");
+let date = new Date();
+document.write(date.getFullYear() - age); 
+
